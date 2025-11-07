678ecfd7a975cd29a5369c8f3392cbcfacf1d618
Yesssssman <yesman1058@gmail.com>
2025-11-07 19:43:32 +0900

    20.13.5
    adopt KeyConflictContext

--- src/main/java/yesman/epicfight/client/input/CombatKeyMapping.java
+++ src/main/java/yesman/epicfight/client/input/CombatKeyMapping.java
@@ -1,20 +1,30 @@
 package yesman.epicfight.client.input;
 
+import org.jetbrains.annotations.NotNull;
+
 import com.mojang.blaze3d.platform.InputConstants;
 
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.settings.KeyConflictContext;
 import yesman.epicfight.client.ClientEngine;
 
 @OnlyIn(Dist.CLIENT)
 public class CombatKeyMapping extends KeyMapping {
+    public CombatKeyMapping(String description, int code, String category) {
+        this(description, InputConstants.Type.KEYSYM, code, category);
+    }
+
+    /**
+     * This key mapping only applies {@link KeyConflictContext#IN_GAME} since it represents player moves
+     */
 	public CombatKeyMapping(String description, InputConstants.Type type, int code, String category) {
-		super(description, type, code, category);
+		super(description, KeyConflictContext.IN_GAME, type, code, category);
 	}
 	
 	@Override
-	public boolean isActiveAndMatches(InputConstants.Key keyCode) {
+	public boolean isActiveAndMatches(@NotNull InputConstants.Key keyCode) {
         return super.isActiveAndMatches(keyCode) && ClientEngine.getInstance().isBattleMode();
     }
 }
--- src/main/java/yesman/epicfight/client/input/EpicFightKeyMappings.java
+++ src/main/java/yesman/epicfight/client/input/EpicFightKeyMappings.java
@@ -5,23 +5,107 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import yesman.epicfight.main.EpicFightMod;
 
 @Mod.EventBusSubscriber(modid = EpicFightMod.MODID, value = Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
 public class EpicFightKeyMappings {
-	public static final KeyMapping WEAPON_INNATE_SKILL_TOOLTIP = new KeyMapping(EpicFightMod.format("key.%s.show_tooltip"), InputConstants.KEY_LSHIFT, EpicFightMod.format("key.%s.gui"));
-	public static final KeyMapping SWITCH_MODE = new KeyMapping(EpicFightMod.format("key.%s.switch_mode"), InputConstants.KEY_R, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping DODGE = new KeyMapping(EpicFightMod.format("key.%s.dodge"), InputConstants.KEY_LALT, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping GUARD = new KeyMapping(EpicFightMod.format("key.%s.guard"), InputConstants.Type.MOUSE, InputConstants.MOUSE_BUTTON_RIGHT, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping ATTACK = new  CombatKeyMapping(EpicFightMod.format("key.%s.attack"), InputConstants.Type.MOUSE, InputConstants.MOUSE_BUTTON_LEFT, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping WEAPON_INNATE_SKILL = new CombatKeyMapping(EpicFightMod.format("key.%s.weapon_innate_skill"), InputConstants.Type.MOUSE, InputConstants.MOUSE_BUTTON_LEFT, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping MOVER_SKILL = new  CombatKeyMapping(EpicFightMod.format("key.%s.mover_skill"), InputConstants.Type.KEYSYM, InputConstants.KEY_SPACE, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping SKILL_EDIT = new KeyMapping(EpicFightMod.format("key.%s.skill_gui"), InputConstants.KEY_K, EpicFightMod.format("key.%s.gui"));
-	public static final KeyMapping LOCK_ON = new KeyMapping(EpicFightMod.format("key.%s.lock_on"), InputConstants.KEY_G, EpicFightMod.format("key.%s.combat"));
-	public static final KeyMapping OPEN_CONFIG_SCREEN = new KeyMapping(EpicFightMod.format("key.%s.config"), -1, EpicFightMod.format("key.%s.gui"));
-	public static final KeyMapping SWITCH_VANILLA_MODEL_DEBUGGING = new KeyMapping(EpicFightMod.format("key.%s.switch_vanilla_model_debug"), -1, EpicFightMod.format("key.%s.system"));
+
+	// Key mappings for GUI
+	public static final KeyMapping WEAPON_INNATE_SKILL_TOOLTIP =
+        new KeyMapping(
+            EpicFightMod.format("key.%s.show_tooltip"),
+            KeyConflictContext.GUI,
+            InputConstants.Type.KEYSYM,
+            InputConstants.KEY_LSHIFT,
+            EpicFightMod.format("key.%s.gui")
+        );
+
+	public static final KeyMapping SKILL_EDIT =
+        new KeyMapping(
+            EpicFightMod.format("key.%s.skill_gui"),
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            InputConstants.KEY_K,
+            EpicFightMod.format("key.%s.gui")
+        );
+
+	public static final KeyMapping OPEN_CONFIG_SCREEN =
+        new KeyMapping(
+            EpicFightMod.format("key.%s.config"),
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            -1,
+            EpicFightMod.format("key.%s.gui")
+        );
+	
+	// Ingame key mappings
+	public static final KeyMapping DODGE =
+        new CombatKeyMapping(
+            EpicFightMod.format("key.%s.dodge"),
+            InputConstants.KEY_LALT,
+            EpicFightMod.format("key.%s.combat")
+        );
+
+	public static final KeyMapping GUARD =
+        new CombatKeyMapping(
+            EpicFightMod.format("key.%s.guard"),
+            InputConstants.Type.MOUSE,
+            InputConstants.MOUSE_BUTTON_RIGHT,
+            EpicFightMod.format("key.%s.combat")
+        );
+
+	public static final KeyMapping ATTACK =
+        new CombatKeyMapping(
+            EpicFightMod.format("key.%s.attack"),
+            InputConstants.Type.MOUSE,
+            InputConstants.MOUSE_BUTTON_LEFT,
+            EpicFightMod.format("key.%s.combat"));
+
+	public static final KeyMapping WEAPON_INNATE_SKILL =
+        new CombatKeyMapping(
+            EpicFightMod.format("key.%s.weapon_innate_skill"),
+            InputConstants.Type.MOUSE,
+            InputConstants.MOUSE_BUTTON_LEFT,
+            EpicFightMod.format("key.%s.combat")
+        );
+
+	public static final KeyMapping MOVER_SKILL =
+        new CombatKeyMapping(
+            EpicFightMod.format("key.%s.mover_skill"),
+            InputConstants.KEY_SPACE,
+            EpicFightMod.format("key.%s.combat")
+        );
+
+    public static final KeyMapping SWITCH_MODE =
+        new KeyMapping(
+            EpicFightMod.format("key.%s.switch_mode"),
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            InputConstants.KEY_R,
+            EpicFightMod.format("key.%s.combat")
+        );
+
+	public static final KeyMapping LOCK_ON =
+        new KeyMapping(
+            EpicFightMod.format("key.%s.lock_on"),
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            InputConstants.KEY_G,
+            EpicFightMod.format("key.%s.combat")
+        );
+	
+	// Systemical key mappings especially for debugging
+	public static final KeyMapping SWITCH_VANILLA_MODEL_DEBUGGING =
+        new KeyMapping(
+            EpicFightMod.format("key.%s.switch_vanilla_model_debug"),
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            -1,
+            EpicFightMod.format("key.%s.system")
+        );
 	
 	@SubscribeEvent
 	public static void registerKeys(RegisterKeyMappingsEvent event) {
