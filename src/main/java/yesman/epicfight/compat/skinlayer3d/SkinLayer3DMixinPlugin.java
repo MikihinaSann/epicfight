07f076d5ea077b9620298d909248e1bc878ec9f7
Ellet <echo.ellet@gmail.com>
2025-12-02 01:20:05 +0300

    refactor: rename SkinLayer3DPlugin -> SkinLayer3DMixinPlugin for consistency with other 'ModMixinPlugin's

--- src/main/java/yesman/epicfight/compat/skinlayer3d/SkinLayer3DMixinPlugin.java
+++ src/main/java/yesman/epicfight/compat/skinlayer3d/SkinLayer3DMixinPlugin.java
@@ -3,7 +3,7 @@ package yesman.epicfight.compat.skinlayer3d;
 import org.jetbrains.annotations.NotNull;
 import yesman.epicfight.compat.ModMixinPlugin;
 
-public final class SkinLayer3DPlugin extends ModMixinPlugin {
+public final class SkinLayer3DMixinPlugin extends ModMixinPlugin {
     @Override
     public @NotNull String getModId() {
         return "skinlayers3d";
