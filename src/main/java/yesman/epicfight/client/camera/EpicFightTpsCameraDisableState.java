a3a599e121bc9516ae77a2484c7c41b4aecb68ee
Ellet <echo.ellet@gmail.com>
2025-12-09 06:48:15 +0300

    fix: avoid changing user's client config when Shoulder Surfing is installed (#2307)

--- src/main/java/yesman/epicfight/client/camera/EpicFightTpsCameraDisableState.java
+++ src/main/java/yesman/epicfight/client/camera/EpicFightTpsCameraDisableState.java
@@ -2,7 +2,7 @@ package yesman.epicfight.client.camera;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import yesman.epicfight.config.ClientConfig;
+import yesman.epicfight.api.client.event.EpicFightClientHooks;
 import yesman.epicfight.main.EpicFightMod;
 
 import java.util.Objects;
@@ -12,12 +12,22 @@ public final class EpicFightTpsCameraDisableState {
     }
 
     private static @Nullable EpicFightTpsCameraDisabledReason reason = null;
+    private static boolean eventRegistered = false;
 
     public static void disable(@NotNull EpicFightTpsCameraDisabledReason reason) {
         Objects.requireNonNull(reason, "reason must not be null");
+
         EpicFightTpsCameraDisableState.reason = reason;
         EpicFightMod.LOGGER.info("Epic Fight TPS mode has been disabled due to a mod conflict with {}", reason.getModName());
-        ClientConfig.cameraMode = ClientConfig.TPSType.ALWAYS_BACK;
+
+        if (!eventRegistered) {
+            EpicFightClientHooks.Camera.ACTIVATE_TPS_CAMERA.registerEvent(e -> {
+                if (EpicFightTpsCameraDisableState.reason != null) {
+                    e.cancel();
+                }
+            });
+            eventRegistered = true;
+        }
     }
 
     public static @Nullable EpicFightTpsCameraDisabledReason getReason() {
