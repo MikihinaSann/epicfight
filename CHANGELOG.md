49f03f0d077c90bd3ef3d5fe2795e9f5ac30ec02
Yesman <79469058+Yesssssman@users.noreply.github.com>
2025-12-10 00:08:42 +0900

    [1.20.1] 8 directional movement in TPS mode (#2314)
    * Added 8 directional movement in TPS camera
    
    * Update EpicFightCameraAPI.java

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Now Lock-on automatically searches a new target if there is no currently focusing entity
+- Now TPS mode applies 8 directional movement to the player
 
 ## [20.14.1] - 2025-12-07
 
--- src/main/java/yesman/epicfight/api/client/event/types/CoupleTPSCamera.java
+++ src/main/java/yesman/epicfight/api/client/event/types/CoupleTPSCamera.java
@@ -52,7 +52,16 @@ public class CoupleTPSCamera extends CameraAPIEvent {
     public boolean shouldCoupleCamera() {
         return this.modified ? this.shouldCouple : this.isMoving || this.isPressingVanillaAttackKeybind || this.isUsingCouplingItem || this.isZooming || this.isHoldingSkill || this.manualCoupling;
     }
-
+    
+    public boolean isOnlyMoving() {
+    	return this.isMoving &&
+			!this.isPressingVanillaAttackKeybind &&
+			!this.isUsingCouplingItem &&
+			!this.isZooming &&
+			!this.isHoldingSkill &&
+			!this.manualCoupling;
+    }
+    
     public boolean isModified() {
         return this.modified;
     }
