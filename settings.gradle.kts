3d3ab7bb20063c78e04f5be21bcd25a2718bdc14
Ellet <echo.ellet@gmail.com>
2025-12-11 00:09:09 +0300

    build(deps): use dev.echoellet.mc-safe-resources from Gradle Portal (https://plugins.gradle.org/plugin/dev.echoellet.mc-safe-resources)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,6 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        // Temporary: required because this plugin is not yet published on the Gradle Plugin Portal
-        //  https://github.com/EchoEllet/minecraft-safe-resources-gradle/issues/1
-        maven { url = uri("https://echoellet.github.io/maven-repo/") }
     }
 }
 
