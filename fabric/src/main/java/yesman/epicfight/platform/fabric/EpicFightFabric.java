d4d4074cfe59cd1fa9635deee911c6a801d95f09
Yesssssman <yesman1058@gmail.com>
2026-01-08 17:25:28 +0900

    Added porting guidance document from `1.20.1` to `1.21.1`

--- fabric/src/main/java/yesman/epicfight/platform/fabric/EpicFightFabric.java
+++ fabric/src/main/java/yesman/epicfight/platform/fabric/EpicFightFabric.java
@@ -3,6 +3,9 @@ package yesman.epicfight.platform.fabric;
 import net.fabricmc.api.ModInitializer;
 import yesman.epicfight.EpicFight;
 
+/// Epic Fight doesn't support Fabric for Minecraft 1.21.1.
+/// This sample project is only for demonstrating the multi-loader structure
+/// and to prepare for future-proofing Epic Fight project to support Fabric.
 public final class EpicFightFabric implements ModInitializer {
     @Override
     public void onInitialize() {
