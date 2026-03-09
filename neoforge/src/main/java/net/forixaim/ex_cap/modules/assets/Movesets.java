1495676e1417f8ed9b3b9e03782d91347343201b
thebe <thebest20067@gmail.com>
2026-03-09 12:01:41 -0700

    compacted the lambda to test, finally fixed the trident issue.

--- neoforge/src/main/java/net/forixaim/ex_cap/modules/assets/Movesets.java
+++ neoforge/src/main/java/net/forixaim/ex_cap/modules/assets/Movesets.java
@@ -324,14 +324,7 @@ public class Movesets
                             Animations.SPEAR_ONEHAND_AIR_SLASH
                     )
                     .setMotionPredicate((entityPatch, interactionHand) ->
-                            {
-                                if (entityPatch.getOriginal().isUsingItem() && entityPatch.getOriginal().getUseItem().getItem() instanceof ProjectileWeaponItem) {
-                                    if (entityPatch.getOriginal().getUseItem().getUseAnimation() == UseAnim.SPEAR) {
-                                        return LivingMotions.AIM;
-                                    }
-                                }
-                                return null;
-                            })
+                            entityPatch.getOriginal().isUsingItem() && entityPatch.getOriginal().getUseItem().getUseAnimation() == UseAnim.SPEAR ? LivingMotions.AIM : null)
                     .addLivingMotionModifier(LivingMotions.AIM, Animations.BIPED_JAVELIN_AIM)
                     .addLivingMotionModifier(LivingMotions.SHOT, Animations.BIPED_JAVELIN_THROW)
                     .addMountAttacks(Animations.SPEAR_MOUNT_ATTACK)
