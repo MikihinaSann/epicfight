ffe949f6246fadf158a8a5efe2ff34e310a6f4fa
thebe <thebest20067@gmail.com>
2026-04-11 22:49:19 -0700

    Removed NetSyncListener due to it requiring an object and clients really don't create listener objects when connecting to a dedicated server.

--- neoforge/src/main/java/yesman/epicfight/api/ex_cap/modules/core/listeners/NetSyncListener.java
+++
@@ -1,7 +0,0 @@
-package yesman.epicfight.api.ex_cap.modules.core.listeners;
-
-import yesman.epicfight.network.server.SPDatapackSync;
-
-//TODO: Find a way to Implement NetSyncListener when all the methods are uhh static...
-public interface NetSyncListener {
-}
