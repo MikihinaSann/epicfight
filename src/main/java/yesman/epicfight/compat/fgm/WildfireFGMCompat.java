ccf57f2a649b929f059b639776dab919373fbdc1
Yesssssman <yesman1058@gmail.com>
2026-04-13 11:40:29 +0900

    Fixed compile errors

--- src/main/java/yesman/epicfight/compat/fgm/WildfireFGMCompat.java
+++ src/main/java/yesman/epicfight/compat/fgm/WildfireFGMCompat.java
@@ -201,10 +201,10 @@ public class WildfireFGMCompat implements ICompatModule {
                     Vector3f translationVector = transform.getTranslation(new Vector3f());
                     poseStack.translate(translationVector.x, translationVector.y, translationVector.z);
                     poseStack.mulPose(transform.getNormalizedRotation(new Quaternionf()).rotateXYZ(Mth.PI, Mth.PI, 0));
-                    matrixStack.translate(0, -0.35, 0.00125);
+                    poseStack.translate(0, -0.35, 0.00125);
 
                     Vector3f scaleVector = transform.getScale(new Vector3f());
-                    matrixStack.scale(scaleVector.x, scaleVector.y, scaleVector.z - 0.0125f);
+                    poseStack.scale(scaleVector.x, scaleVector.y, scaleVector.z - 0.0125f);
 
                     if (bounceEnabled) {
                         poseStack.translate(totalX / 32.0F, 0.0F, 0.0F);
